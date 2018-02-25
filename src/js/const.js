60fe1a0a92be777d896e4509465c90a7b23e0c7a
Steven Mao <31294078+nzrugby@users.noreply.github.com>
2018-02-26 00:50:34 +1300

    add instructions array

--- src/js/const.js
+++ src/js/const.js
@@ -1,5 +1,6 @@
 //Dir: direction
 const Dir = ['NORTH','EAST','SOUTH','WEST'];
+const INSTRUCTIONS = ['LEFT','RIGHT','MOVE','REPORT'];
 const ANGLE = {'NORTH':0,'EAST':-90,'SOUTH':180,'WEST':90};
 //X direction
 const EAST_BORDER = 6;
@@ -8,4 +9,4 @@ const WEST_BORDER = 2;
 const NORTH_BORDER = 2;
 const SOUTH_BORDER = 6;
 const pattern = /^PLACE\s{1}\d,\d,(NORTH)|(EAST)|(WEST)|(SOUTH)$/;
-export {instructions,Dir,EAST_BORDER,NORTH_BORDER,SOUTH_BORDER,WEST_BORDER,pattern,ANGLE}
+export {instructions,Dir,EAST_BORDER,NORTH_BORDER,SOUTH_BORDER,WEST_BORDER,pattern,ANGLE,INSTRUCTIONS}
