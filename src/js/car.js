ef63905ff6779cea1f66f0bd2c42a6fd7554c05a
Steven Mao <31294078+nzrugby@users.noreply.github.com>
2018-02-26 00:08:44 +1300

    fixed some bugs

--- src/js/car.js
+++ src/js/car.js
@@ -1,14 +1,14 @@
-import {Dir,EAST_BORDER,NORTH_BORDER,SOUTH_BORDER,WEST_BORDER} from './const.js'
+import {Dir,EAST_BORDER,NORTH_BORDER,SOUTH_BORDER,WEST_BORDER,ANGLE} from './const.js'
 import {output} from './ui.js'
 
 var Car = function(){
-    //initial value
-    var X = 0,Y = 0, direction = "NORTH";
-    //real distance for car to move in webpage
+    var X = 0,Y = 0;
+    var direction = "NORTH";
+    // 
     var cellLength = 42;
-    //for car rotation
+    // for car rotation
     var angle = 0;
-    //to judge if bus is inside carpark
+    // to judge if bus is inside carpark
     var insideCarPark = false;
     var car = document.querySelector('.car');
 
@@ -19,87 +19,83 @@ var Car = function(){
         angle = 0;
         car.style.transform = `rotate(${angle}deg)`;
     }
-    
     // accept instruction from control system
     this.instruction = function(cmd){ 
         console.log(cmd);
         var instrs = cmd.replace(" ",",").split(",");
         if(instrs.indexOf('PLACE')>=0){
-            place(instrs[1],instrs[2],instrs[3])
+            place(parseInt(instrs[1]),parseInt(instrs[2]),instrs[3])
         }
-        else{
+        else if(Dir.indexOf(cmd)>=0){
             var fun = insideCarPark? cmd.toLowerCase()+"()":"";
             eval(fun);
         }
         showReport(cmd);
     }
-    
     // private function to show real-time data and report
     function showReport(cmd){
         output.innerHTML = `Real-time data:<br>
                             Current instruction:  ${cmd}<br>
-                            Current X position:  ${X}<br>
-                            Current Y position:  ${Y}<br>
+                            Current X position:  ${X-2}<br>
+                            Current Y position:  ${6-Y}<br>
                             Current Direction:  ${direction}<br>
-                            Report:  ${cmd.indexOf('REPORT')>=0? [X,Y,direction]:""}
+                            Report:  ${cmd.indexOf('REPORT')>=0? [X-2,6-Y,direction]:""}
                             `;
     }
-    
-    // execute PLACE cmd. d represent direction like "WEST" .etc.
+    // execute PLACE cmd
     function place(x,y,d){
-        X = parseInt(x);
-        Y = parseInt(y);
-        if(isInsidCarPark(X,Y)){
+        if(isInsideCarPark(x+2,6-y)){
+            X = x+2;
+            Y = 6-y;
+            angle = ANGLE[direction];
             insideCarPark = true;
             setCarPosition(X,Y,d);
         }
     }
-    
     // set car position. called by reset and place 
     function setCarPosition(x,y,d){
         direction = d;
         car.style.top = y*cellLength + 'px';
         car.style.left = x*cellLength + 'px';
+        car.style.transform = `rotate(${angle}deg)`;
     }
-    
-    function isInsidCarPark(x,y){
+
+    function isInsideCarPark(x,y){
         return x>= WEST_BORDER && x<=EAST_BORDER && y>=NORTH_BORDER && y<=SOUTH_BORDER
     }
-    
+
     // execute LEFT cmd
     function left(){
         angle -= 90;
         direction = Dir[(Dir.indexOf(direction)+3)%4];
         car.style.transform = `rotate(${angle}deg)`;
     }
-    
     // execute RIGHT cmd
     function right(){
         angle +=90;
         direction = Dir[(Dir.indexOf(direction)+1)%4];
         car.style.transform = `rotate(${angle}deg)`;
     }
-    
     // execute MOVE cmd
     function move(){
-        var p = {x:X,y:Y};
+        var p = {x:X,y:Y,ang:angle};
         switch(direction){
-            case 'NORTH': p.y--;break;
-            case 'SOUTH': p.y++;break;
-            case 'EAST' : p.x++;break;
-            case 'WEST' : p.x--;break;
-            default: break;
+            case 'NORTH': p.y--;p.ang=0;break;
+            case 'SOUTH': p.y++;p.ang=180;break;
+            case 'EAST' : p.x++;p.ang=-90;break;
+            case 'WEST' : p.x--;p.ang=90;break;
+            default:break;
         }
-        if(insideCarPark(x,y)){
+        if(isInsideCarPark(p.x,p.y)){
             X = p.x;
             Y = p.y;
+            angle = p.ang;
             setCarPosition(X,Y,direction)
         }
     }
-    
-    // execute REPORT cmd
+     // execute REPORT cmd
     function report(){
-        console.log( [X,Y,direction] );
+        console.log( [X-2,6-Y,direction] );
     }
 }
 
