3c8bfaf766bbe699be135339480bbd7d48df25c9
Steven Mao <31294078+nzrugby@users.noreply.github.com>
2018-02-26 00:17:28 +1300

    modify pattern to concisely match the instruction

--- src/js/const.js
+++ src/js/const.js
@@ -1,10 +1,11 @@
 //Dir: direction
 const Dir = ['NORTH','EAST','SOUTH','WEST'];
+const ANGLE = {'NORTH':0,'EAST':-90,'SOUTH':180,'WEST':90};
 //X direction
 const EAST_BORDER = 6;
 const WEST_BORDER = 2;
 //Y direction
 const NORTH_BORDER = 2;
 const SOUTH_BORDER = 6;
-const pattern = /^PLACE \d,\d,(NORTH)|(EAST)|(WEST)|(SOUTH)$/
-export {instructions,Dir,EAST_BORDER,NORTH_BORDER,SOUTH_BORDER,WEST_BORDER,pattern}
+const pattern = /^PLACE\s{1}\d,\d,(NORTH)|(EAST)|(WEST)|(SOUTH)$/;
+export {instructions,Dir,EAST_BORDER,NORTH_BORDER,SOUTH_BORDER,WEST_BORDER,pattern,ANGLE}
