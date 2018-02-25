3bd1d3adf421a7f74080fc37612e9e4c9154e00c
Steven Mao <31294078+nzrugby@users.noreply.github.com>
2018-02-25 17:15:20 +1300

    Update car.js
    add setCarPosition( ) function and isInsideCarPark( ) function

--- src/js/car.js
+++ src/js/car.js
@@ -2,20 +2,24 @@ import {Dir,EAST_BORDER,NORTH_BORDER,SOUTH_BORDER,WEST_BORDER} from './const.js'
 import {output} from './ui.js'
 
 var Car = function(){
-    var X = 0,Y = 0;
-    var direction = "NORTH";
+    //initial value
+    var X = 0,Y = 0, direction = "NORTH;
+    //real distance for car to move in webpage
     var cellLength = 42;
+    //for car rotation
     var angle = 0;
+    //to judge if bus is inside carpark
     var insideCarPark = false;
     var car = document.querySelector('.car');
 
     // every time when start button clicked the car will be reseted to original location and status
     this.reset = function(){
-        place(0,0,"NORTH");
         insideCarPark = false;
+        setCarPosition(0,0,"NORTH");
         angle = 0;
         car.style.transform = `rotate(${angle}deg)`;
     }
+    
     // accept instruction from control system
     this.instruction = function(cmd){ 
         console.log(cmd);
@@ -29,6 +33,7 @@ var Car = function(){
         }
         showReport(cmd);
     }
+    
     // private function to show real-time data and report
     function showReport(cmd){
         output.innerHTML = `Real-time data:<br>
@@ -39,29 +44,42 @@ var Car = function(){
                             Report:  ${cmd.indexOf('REPORT')>=0? [X,Y,direction]:""}
                             `;
     }
+    
     // execute PLACE cmd
     function place(x,y,d){
         X = parseInt(x);
         Y = parseInt(y);
-        direction = d;
-        car.style.top = Y*cellLength + 'px';
-        car.style.left = X*cellLength + 'px';
-        if(X>= WEST_BORDER && X<=EAST_BORDER && Y>=NORTH_BORDER && Y<=SOUTH_BORDER){
+        if(isInsidCarPark(X,Y)){
             insideCarPark = true;
+            setCarPosition(X,Y,d);
         }
     }
+    
+    // set car position. called by reset and place 
+    function setCarPosition(x,y,d){
+        direction = d;
+        car.style.top = y*cellLength + 'px';
+        car.style.left = x*cellLength + 'px';
+    }
+    
+    function isInsidCarPark(x,y){
+        return x>= WEST_BORDER && x<=EAST_BORDER && y>=NORTH_BORDER && y<=SOUTH_BORDER
+    }
+    
     // execute LEFT cmd
     function left(){
         angle -= 90;
         direction = Dir[(Dir.indexOf(direction)+3)%4];
         car.style.transform = `rotate(${angle}deg)`;
     }
+    
     // execute RIGHT cmd
     function right(){
         angle +=90;
         direction = Dir[(Dir.indexOf(direction)+1)%4];
         car.style.transform = `rotate(${angle}deg)`;
     }
+    
     // execute MOVE cmd
     function move(){
         var step = 1;
@@ -73,6 +91,7 @@ var Car = function(){
             default: break;
         }
     }
+    
      // execute REPORT cmd
     function report(){
         console.log( [X,Y,direction] );
