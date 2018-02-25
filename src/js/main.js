b637ee7f140dc25e590e3244cd2b4426070c61f2
Steven Mao <31294078+nzrugby@users.noreply.github.com>
2018-02-26 00:15:14 +1300

    import test data from json file

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,30 @@
 import '../css/buspark.css'
 import Car from './car.js'
-import {instructions} from './const.js'
-import {busArea} from './ui.js'
-		
+import {pattern} from './const.js'
+import {busArea,testOptions} from './ui.js'
+import instructionTests from './instructions.json'		
 
 var car = new Car();
-	
+
 var btn = document.getElementById('btnStart');
 btn.addEventListener('click',()=>{
+	var testIndex = getTestOption();
 	car.reset();
-	StartPark();
+	var instructions = preFilterInstructions(instructionTests[testIndex])
+	startPark(instructions);
 })
 
-function StartPark(){
+function getTestOption(){
+	var index = testOptions.selectedIndex;
+	return testOptions.options[index].value;
+}  
+
+function preFilterInstructions(instructions){
+	return instructions.split(";"); 
+}
+
+function startPark(instructions){
 	// use regex to find the first valid PLACE cmd in cmd sequence
-	var pattern = /^PLACE \d,\d,(NORTH)|(EAST)|(WEST)|(SOUTH)$/
 	while(!pattern.test(instructions[0])){
 		// take instructions as queue and always remove the first element if it's not right one'
 		instructions.shift();
